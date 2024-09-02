e85d3172fb3ff6497a1a5d88c246bbf276a1f9d2
Adarsh Kumar <adarashkumar9002@gmail.com>
2024-09-02 18:04:51 +0530

    fixed the html and removed script for rety the logic

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+// Import
+
+import axios from "./node_modules/axios/dist/esm/axios.js";
+
+// Elements
 const DROPDOWN_ELEMENT = document.getElementById("countryData");
 const LIST_ELEMENT = document.getElementById("list");
 
+// Variables
+const API_LINK =
+  "https://raw.githubusercontent.com/AdarshKumar9002/conutry-city-detail/main/data.json";
+
+
+  // Fetching Data
 async function fetchData() {
-  const request = await fetch(
-    "https://raw.githubusercontent.com/AdarshKumar9002/conutry-city-detail/main/data.json"
-  );
-  const response = await request.json();
+  let response = await axios.get(API_LINK);
+  response = response.data;
   return response;
 }
 
+// Creating Option Element HTML
 const createDropdownList = (key, index) => {
   const DROPDOWN_LIST_ELEMENT = document.createElement("option");
   DROPDOWN_LIST_ELEMENT.id = `option${index}`;
@@ -17,88 +27,24 @@ const createDropdownList = (key, index) => {
   DROPDOWN_ELEMENT.appendChild(DROPDOWN_LIST_ELEMENT);
 };
 
-const storeKeys = async () => {
-  const data = await fetchData();
-  let totalKeys = [];
 
-  data.forEach((item) => {
-    let key = Object.keys(item)[0];
-    if (!totalKeys.includes(key)) {
-      totalKeys.push(key);
-    }
-  });
+// Get all countries names
 
-  return totalKeys;
-};
+// Get all the city name bassed on country 
 
-const populateDropdownList = async () => {
-  const keys = await storeKeys();
-  keys.forEach((key, index) => {
-    createDropdownList(key, index + 1);
-  });
-};
+// create array of city name
 
-const checkExistedKeys = (keys, countryName, obj, cities) => {
-  if (keys.includes(countryName)) {
-    if (!obj[countryName]) {
-      obj[countryName] = [];
-    }
-    // Use concat to merge the cities array into obj[countryName]
-    obj[countryName] = obj[countryName].concat(cities);
-  }
-};
+// Check for Duplicate and store only one copy
 
-const getCitiesName = async () => {
-  const data = await fetchData();
-  const keys = await storeKeys();
+// Populate the Dropdown 
 
-  const obj = {};
+// Create a eventlistner for btn
 
-  data.map((item) => {
-    const countryName = Object.keys(item)[0];
-    const cities = Object.values(item)[0];
-    console.log(Object.values(item)[0]);
+// Show the History text
 
-    checkExistedKeys(keys, countryName, obj, cities);
-  });
-  console.log(obj);
 
-  return obj;
-};
 
-const createListElements = (city) => {
-    const LIST_ITEM_ELEMENT = document.createElement("li");
-    LIST_ITEM_ELEMENT.classList.add(
-      "text-gray-700",
-      "text-lg",
-      "font-semibold"
-    );
-    LIST_ITEM_ELEMENT.textContent = city;
-    LIST_ELEMENT.appendChild(LIST_ITEM_ELEMENT);
-  };
-  
 
-const populateList = async (selectedCountry) => {
-  const data = await getCitiesName();
-  const cities = data[selectedCountry];
-  if (cities) {
-    cities.forEach((city) => {
-      createListElements(city);
-    });
-  }
-};
 
-const handleDropdownChange = () => {
-  LIST_ELEMENT.innerHTML = ""; // Clear previous list
-  const selectedOption = DROPDOWN_ELEMENT.value;
-  if (selectedOption === "Select a Country") return;
 
-  if (selectedOption) {
-    populateList(selectedOption);
-  }
-};
 
-document.addEventListener("DOMContentLoaded", async () => {
-  await populateDropdownList();
-  DROPDOWN_ELEMENT.addEventListener("change", handleDropdownChange);
-});
