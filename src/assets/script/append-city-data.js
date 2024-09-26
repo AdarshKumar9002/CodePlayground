aea8270d74524ee3c2812438a4ae4bb85ef0b6ce
Adarsh Kumar <adarashkumar9002@gmail.com>
2024-09-27 01:12:01 +0530

    removed the find button now result shown on dropdown selection

--- src/assets/script/append-city-data.js
+++ src/assets/script/append-city-data.js
@@ -6,7 +6,6 @@ class AppendCityData {
     constructor() {
         this.COUNTRY_DROPDOWN_ELEMENT = document.getElementById("countryData");
         this.CITY_DROPDOWN_ELEMENT = document.getElementById("cityData");
-        this.FIND_HISTORY_BTN_ELEMENT = document.getElementById("findHistoryBtn");
         this.cityNames = new GetCityName();
         this.selectedCountry = "";
         this.selectedCity = "";
@@ -33,7 +32,6 @@ class AppendCityData {
             "afterbegin",
             `<option>Select a City</option>`
         );
-        this.FIND_HISTORY_BTN_ELEMENT.setAttribute("disabled", "true"); // Disable button initially
     }
 
     static async renderCityCards(selectedCountry) {
@@ -47,11 +45,6 @@ class AppendCityData {
             this.resetDropdown();
             this.appendCityOptions(this.selectedCountry);
             AppendCityData.renderCityCards(this.selectedCountry);
-        });
-
-        this.CITY_DROPDOWN_ELEMENT.addEventListener("change", () => {
-            const citySelected = this.CITY_DROPDOWN_ELEMENT.value !== "Select a City";
-            this.FIND_HISTORY_BTN_ELEMENT.disabled = !citySelected; 
         });
     }
 }
