03ce0ea16db5ad766c988247ed53581bc28afb01
Adarsh <adarashkumar9002@gmail.com>
2024-10-19 18:21:01 +0530

    refactored render-card.js

--- src/assets/script/render-cards.js
+++ src/assets/script/render-cards.js
@@ -1,34 +1,33 @@
 import CityCards from "./append-city-cards.js";
 
-const COUNTRY_DROPDOWN_ELEMENT = document.getElementById("countryData");
-const CITY_DROPDOWN_ELEMENT = document.getElementById("cityData");
+const COUNTRY_DROPDOWN_ELEMENT = $("#countryData");
+const CITY_DROPDOWN_ELEMENT = $("#cityData");
 
 const cityCards = new CityCards();
 
 const renderCards = () => {
   if (
-    COUNTRY_DROPDOWN_ELEMENT.value === "Select a Country" ||
-    COUNTRY_DROPDOWN_ELEMENT.value === "" ||
-    COUNTRY_DROPDOWN_ELEMENT.value === null
+    COUNTRY_DROPDOWN_ELEMENT.val("Select a Country") ||
+    COUNTRY_DROPDOWN_ELEMENT.val("") ||
+    COUNTRY_DROPDOWN_ELEMENT.val(null)
   ) {
     cityCards.renderAllCards();
   } else {
-    const selectedCountry = COUNTRY_DROPDOWN_ELEMENT.value;
+    const selectedCountry = COUNTRY_DROPDOWN_ELEMENT.val();
     cityCards.renderSelctedCoutnryCards(selectedCountry);
   }
 };
 
 const renderSelectedCityCard = () => {
   if (
-    CITY_DROPDOWN_ELEMENT.value !== "Select a City" ||
-    CITY_DROPDOWN_ELEMENT.value !== "" ||
-    CITY_DROPDOWN_ELEMENT.value !== null
+    CITY_DROPDOWN_ELEMENT.val("Select a City") ||
+    CITY_DROPDOWN_ELEMENT.val("") ||
+    CITY_DROPDOWN_ELEMENT.val(null)
   ) {
-    const selectedCity = CITY_DROPDOWN_ELEMENT.value;
+    const selectedCity = CITY_DROPDOWN_ELEMENT.val();
     cityCards.renderSelectedCityCard(selectedCity);
   }
 };
-
 
 export {
   CITY_DROPDOWN_ELEMENT,
