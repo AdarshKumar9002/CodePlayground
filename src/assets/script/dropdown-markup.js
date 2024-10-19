1481a634e642ca3f657fed70517cda56dc021c78
Adarsh <adarashkumar9002@gmail.com>
2024-10-19 17:47:09 +0530

    refactored dropdown-markup.js

--- src/assets/script/dropdown-markup.js
+++ src/assets/script/dropdown-markup.js
@@ -7,6 +7,13 @@ class DropdownOptionHtml {
       dropdownElement.appendChild(DROPDOWN_OPTION_ELEMENT);
       return DROPDOWN_OPTION_ELEMENT;
     }
+    static dropdownOption(dropdownElement, countryName) {
+      const DROPDOWN_OPTION_ELEMENT = $("<option> </option>");
+      DROPDOWN_OPTION_ELEMENT.val(countryName);
+      DROPDOWN_OPTION_ELEMENT.text(countryName);
+      dropdownElement.append(DROPDOWN_OPTION_ELEMENT);
+      return DROPDOWN_OPTION_ELEMENT;
+    }
   }
   
   export default DropdownOptionHtml;
